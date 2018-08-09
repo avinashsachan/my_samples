2b6a9351275a0eb043806da7e37c0717b7eb028c
Avinash <avinashsachan@gmail.com>
2018-08-09 22:23:57 +0530

    Adding test

--- date_duration.py
+++ date_duration.py
@@ -3,33 +3,34 @@
 from datetime import timedelta
 from datetime import datetime
 
+class date_duration():
+    @staticmethod
+    def GetDelta(st, et):
+        dct = {}
+        tDelta = timedelta(hours=24)
 
-def GetDelta(st, et):
-    dct = {}
-    tDelta = timedelta(hours=24)
-
-    nextDay = st.date().__add__(tDelta)
-    nextDate = datetime.combine(nextDay, datetime.min.time())
-
-    endDate = datetime.combine(et.date().__add__(tDelta), datetime.min.time())
-
-    # if nextDate > et:
-    #    dct[st.date()] = (et - st).total_seconds() / 60
-    # else:
-    while nextDate <= endDate:
-        t = (min(nextDate, et) - st).total_seconds() / 60
-        if t != 0:
-            dct[st.date()] = t
-
-        st = nextDate
-        nextDay = nextDay.__add__(tDelta)
+        nextDay = st.date().__add__(tDelta)
         nextDate = datetime.combine(nextDay, datetime.min.time())
 
-    # p1 = et.date() - st.date()
-    #print("Starting")
-    #print("ST = ", st)
-    #print("ET = ", et)
+        endDate = datetime.combine(et.date().__add__(tDelta), datetime.min.time())
 
-    return dct
+        # if nextDate > et:
+        #    dct[st.date()] = (et - st).total_seconds() / 60
+        # else:
+        while nextDate <= endDate:
+            t = (min(nextDate, et) - st).total_seconds() / 60
+            if t != 0:
+                dct[st.date()] = t
+
+            st = nextDate
+            nextDay = nextDay.__add__(tDelta)
+            nextDate = datetime.combine(nextDay, datetime.min.time())
+
+        # p1 = et.date() - st.date()
+        #print("Starting")
+        #print("ST = ", st)
+        #print("ET = ", et)
+
+        return dct
 
 
